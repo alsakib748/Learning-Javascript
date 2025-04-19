5f4cedb6760e4ba4b85117e92ec40e09a94e697b
alsakib748 <alsakib748@gmail.com>
2025-04-19 22:54:22 +0600

    Javascript Dom

---
+++ 07. JavaScript in the Browser DOM and Events PROJECT/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// console.log(document.querySelector(".message"));
+
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".message").textContent = "Correct Number!";
+
+// console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".score").textContent = 20;
+
+const guess_value = document.querySelector(".guess").value;
+
+console.log(guess_value);
